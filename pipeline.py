d683474bc56f9c41a8a4ee7b813b773832c09d0b
Jasper Alblas <just_jasper@hotmail.com>
2023-10-31 15:24:07 +0100

    Complete part 1

--- pipeline.py
+++ pipeline.py
@@ -4,24 +4,29 @@ A pipeline which fetches data from an API, transforms the data, and uploads it i
 
 import requests
 import pandas as pd
-import sqlalchemy
+from sqlalchemy import create_engine
 
 
 def fetch_data():
     """ 
     Fetches data from https://api.energidataservice.dk/meta/dataset/Gasflow into a variable
     """
-    url = 'https://api.energidataservice.dk/meta/dataset/Gasflow'
+    url = 'https://api.energidataservice.dk/dataset/PowerSystemRightNow?limit=100'
     data = requests.get(url).json()
-    return data["columns"]
+    return data["records"]
 
 
 def transform(data):
     """ Transforms the dataset"""
-    print(data)
     df = pd.DataFrame(data)
     print(f"Total number of rows {len(data)}")
     return df
+
+
+def ingest(df):
+    """ Ingests data into sqllite database"""
+    engine = create_engine('sqlite:///data.db')
+    df.to_sql('energy', engine, if_exists='replace')
 
 
 def main():
@@ -29,7 +34,9 @@ def main():
     Calls the different functions
     '''
     json = fetch_data()
-    transform(json)
+    df = transform(json)
+    print(df)
+    ingest(df)
 
 
 if __name__ == "__main__":
